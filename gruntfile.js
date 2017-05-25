1f2ff0b547fd4a8673c8f70d463eb46fb018cc5e
Temi Adeyeri <temi.adeyeri@gmail.com>
2017-05-24 22:16:22 -0500

    .

--- gruntfile.js
+++ gruntfile.js
@@ -8,22 +8,66 @@ module.exports = function (grunt) {
             expand: true,
             cwd: "./public",
             src: ["**"],
-            dest: "./dist/server/public"
+            dest: "./build/public"
           },
           {
             expand: true,
             cwd: "./views",
             src: ["**"],
-            dest: "./dist/server/views"
+            dest: "./build/views"
+          }
+        ]
+      },
+      deploy: {
+        files: [{
+            expand: true,
+            cwd: "./public",
+            src: ["**"],
+            dest: "./deploy/build/public"
+          },
+          {
+            expand: true,
+            cwd: "./views",
+            src: ["**"],
+            dest: "./deploy/build/views"
+          },
+          {
+            expand: true,
+            cwd: "./",
+            src: ["package.json"],
+            dest: "./deploy"
+          },
+          {
+            expand: true,
+            cwd: "./bin",
+            src: ["**"],
+            dest: "./deploy/bin"
           }
         ]
       }
     },
+
     ts: {
       app: {
         files: [{
-          src: ["src/\*\*/\*.ts", "!src/.baseDir.ts"],
-          dest: "./dist"
+        fast:false,
+          src: ["./src/\*\*/\*.ts", "!src/.baseDir.ts"],
+          dest: "./build"
+        }],
+        options: {
+          "module": "commonjs",
+          "target": "es6",
+          "noImplicitAny": false,
+          "sourceMap": true,
+          "experimentalDecorators": true,
+          "emitDecoratorMetadata": true,
+         // sourceRoot: "./src/"
+        }
+      },
+      deploy: {
+        files: [{
+          src: ["./src/\*\*/\*.ts", "!src/.baseDir.ts"],
+          dest: "./deploy/build"
         }],
         options: {
           "module": "commonjs",
@@ -37,7 +81,7 @@ module.exports = function (grunt) {
     },
     watch: {
       ts: {
-        files: ["src/**/*.ts"],
+        files: ["src/\*\*/\*.ts"],
         tasks: ["ts"]
       },
       views: {
@@ -51,13 +95,18 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-ts");
 
-  grunt.registerTask("default", [
-    "copy",
-    "ts"
+  grunt.registerTask("deploy", [
+    "ts:deploy",
+    "copy:deploy"
+  ]);
+  grunt.registerTask("debug:build", [
+    "ts:app",
+    "copy:build",
+    "watch"
   ]);
   grunt.registerTask("ts-w", [
-    "copy",
     "ts",
+    "copy",
     "watch"
   ]);
 
