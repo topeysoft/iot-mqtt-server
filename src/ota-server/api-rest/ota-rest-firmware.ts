798bfa4a73db4e3c70a682e19e2093a72eae73dd
Temi Adeyeri <temi.adeyeri@gmail.com>
2017-06-15 12:38:13 -0500

    Wrote some unit test for api-rest-firsmwares

--- src/ota-server/api-rest/ota-rest-firmware.ts
+++ src/ota-server/api-rest/ota-rest-firmware.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import {Express, Request, Response} from 'express';
+import { Express, Request, Response } from 'express';
 import { Repository } from "./../../repository/repository.diskdb";
 import { INTERNAL_SERVER_ERROR, BAD_REQUEST } from "http-status-codes";
 import { ObjectID } from "mongodb";
@@ -12,21 +12,23 @@ export class FirmwareApiRoutes {
      *
      */
 
-    static getApp():Express{
-       var api = new FirmwareApiRoutes();
-        return  api.expressApp;
+
+    static getApp(): Express {
+        var api = new FirmwareApiRoutes();
+        return api.expressApp;
     };
     constructor() {
-        this.basePath = `/firmwares`;
         this.expressApp = express.Router();
-        this.setup();
     }
 
-    private expressApp:any;
-    private basePath;
+    init() {
+        this.setup();
+    }
+    public expressApp: any;
+    public basePath = '/firmwares';
 
     public setup() {
-        this.expressApp.get(`${this.basePath}`, (req:Request, resp) => {
+        this.expressApp.get(`${this.basePath}`, (req: Request, resp) => {
             return this.getFirmwares(req, resp);
         });
         this.expressApp.get(`${this.basePath}/:firmware_id`, (req, resp) => {
@@ -57,8 +59,8 @@ export class FirmwareApiRoutes {
     }
     getFirmwareById(req: Request, resp: Response) {
         try {
-            let id= new ObjectID(req.params["firmware_id"])
-             Repository.getOne('firmwares', { _id:id })
+            let id = new ObjectID(req.params["firmware_id"])
+            Repository.getOne('firmwares', { _id: id })
                 .then((data) => {
                     resp.send(data);
                 })
---
+++ tests/ota-server/api-rest/api-rest-firmwares.test.ts
@@ -0,0 +1,48 @@
+import * as express from 'express';
+var expect = require('expect.js');
+var assert = require('chai').assert;
+var sinon = require('sinon');
+import 'mocha';
+import { OtaRestApi } from '../../../src/ota-server/api-rest/ota-rest-app';
+import { FirmwareApiRoutes } from "../../../src/ota-server/api-rest/ota-rest-firmware";
+
+describe("FirmwareApiRoutes", () => {
+    
+    describe('Constructor', () => {
+        let appInstance = new FirmwareApiRoutes;
+        it('should set base_path to /firmwares', () => {
+            assert.equal(appInstance.basePath, '/firmwares');
+        })
+        it('should set set expressApp to new instance of express App', () => {
+            expect(appInstance.expressApp).to.be.ok();
+        })
+    })
+    describe('init', ()=>{
+        it('should call setup', ()=>{
+            let appInstance = new FirmwareApiRoutes;
+            let spy = sinon.spy(appInstance, 'setup');
+            appInstance.init();
+            assert.isTrue(spy.calledOnce);
+        })
+    });
+    describe('setup', ()=>{
+        it('should call expressApp get with basePath', ()=>{
+            let appInstance = new FirmwareApiRoutes;
+            let spy = sinon.spy(appInstance.expressApp, 'get');
+            appInstance.setup();
+            assert.isTrue(spy.withArgs(appInstance.basePath).calledOnce);
+        })
+        it('should call getFirmwares', ()=>{
+            let appInstance = new FirmwareApiRoutes;
+            let spy = sinon.spy(appInstance.getFirmwares, 'get');
+            appInstance.setup();
+            assert.isTrue(spy.calledOnce);
+        })
+        it('should call expressApp get with basePath and :id_param', ()=>{
+            let appInstance = new FirmwareApiRoutes;
+            let spy = sinon.spy(appInstance.expressApp, 'get');
+            appInstance.setup();
+            assert.isTrue(spy.withArgs(`${appInstance.basePath}/:firmware_id`).calledOnce);
+        })
+    })
+}) 
